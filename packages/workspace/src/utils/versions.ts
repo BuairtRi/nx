10ad855c005de1db105117a94392456be800d662
Victor Savkin <vic.savkin@gmail.com>
2019-04-21 11:15:49 -0400

    fix: add missing webpack deps to node and web

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -4,3 +4,4 @@ export const angularCliVersion = '7.3.1';
 export const typescriptVersion = '3.2.2';
 export const prettierVersion = '1.16.4';
 export const jasmineMarblesVersion = '^0.4.0';
+export const angularVersion = '^7.0.0';
